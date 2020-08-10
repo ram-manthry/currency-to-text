cc0a8d863d07e1be226241def7cc207c1c76dd86
Ram Manthry <ram@farmote.com>
2020-08-11 02:34:39 +1200

    started with some basic test cases

---
+++ CurrencyService.Tests/CurrencyService.Tests.cs
@@ -0,0 +1,46 @@
+using NUnit.Framework;
+using FluentAssertions;
+
+namespace CurrencyService.Tests
+{
+    public class CurrencyServiceTests
+    {
+        private CurrencyService _currencyService;
+        [SetUp]
+        public void Setup()
+        {
+            _currencyService = new CurrencyService();
+        }
+
+        [TestCase(0, "Zero")]
+        [TestCase(1, "One")]
+        [TestCase(9, "Nine")]
+        public void When_Single_Digit_Then_Return_From_Dictionary(int input, string expected)
+        {
+            var actual = _currencyService.ConvertToText(input);
+            actual.Should().Be(expected);
+        }
+
+        [TestCase(11, "Eleven")]
+        [TestCase(19, "Nineteen")]
+        public void When_Two_Digits_Below_20_Then_Return_From_Dictionary(int input, string expected) {
+            var actual = _currencyService.ConvertToText(input);
+            actual.Should().Be(expected);
+        }
+
+        [TestCase(10, "Ten")]
+        [TestCase(20, "Twenty")]
+        [TestCase(50, "Fifty")]
+        [TestCase(90, "Ninety")]
+        public void When_Two_Digits_Multiples_Of_10_Then_Return_From_Dictionary(int input, string expected) {
+            var actual = _currencyService.ConvertToText(input);
+            actual.Should().Be(expected);
+        }
+
+        [TestCase(21, "Twenty One")]
+        public void When_Two_Digits_Above_20_And_Not_10_Multiples(int input, string expected) {
+            var actual = _currencyService.ConvertToText(input);
+            actual.Should().Be(expected);
+        }
+    }
+}
---
+++ CurrencyService/CurrencyService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CurrencyService
+{
+    public class CurrencyService
+    {
+        public string ConvertToText(int number) {
+            var text = string.Empty;
+            text = CurrencyLookup.NumberTexts[number];
+            return text;
+        }
+    }
+}
